185cb5af4a9221025b3cc1999651e30938b7386e
Jesse Luehrs <doy@tozt.net>
2014-10-27 12:20:20 -0400

    draw into our own window

--- examples/mandelbrot.jl
+++ examples/mandelbrot.jl
@@ -7,5 +7,12 @@ using FractalExplorer
 iterations = 45
 imgsize = (640, 480)
 
-imgc, imgslice = view([ HSV(0, 0, 0) for y=1:imgsize[2], x=1:imgsize[1] ])
-mandelbrot(imgc, iterations)
+win = Toplevel("FractalExplorer", imgsize[1], imgsize[2], false)
+frame = Frame(win)
+pack(frame, expand=true, fill="both")
+canvas = Canvas(frame, imgsize[1], imgsize[2])
+pack(canvas, expand=true, fill="both")
+set_visible(win, true)
+view(canvas, [ 0.0 for y=1:imgsize[2], x=1:imgsize[1] ], interactive=false)
+
+mandelbrot(canvas, iterations)
